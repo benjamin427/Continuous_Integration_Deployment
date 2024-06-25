d4fae33eb222fc481d4282bd04062237f82fdf16
Benjamin_79 <shadowboxer79@hotmail.com>
2024-06-25 12:34:17 -0400

    Configurations

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const createBuilder = require('@bahmutov/cypress-esbuild-preprocessor')
 const addCucumberpreprocessor = require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin
 const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin
 
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
